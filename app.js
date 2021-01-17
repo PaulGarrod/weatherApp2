5ca786196c812144ccf4b42e475f0298fb9fb2c9
Paul Garrod <58899433+PaulGarrod@users.noreply.github.com>
2021-01-17 15:32:09 +1300

    update API
    API fully linked. Info displaying.

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const temp = document.querySelector('#temp');
 const wind = document.querySelector('#wind');
 const visability = document.querySelector('#visability');
 
-const weatherDataMain = getData().then ((data) => {
-    return data.weather.main
-});
-const weatherDataDescription = getData().then ((data) => {
-    return data.weather.description
+//convert temp to oC
+
+//convert wind speed?
+
+//convert
+
+const renderWeatherData = getData().then((data) => {
+    city.innerText = `${data.name}, ${data.sys.country}`;
+    weather.innerText = `Current Weather: ${data.weather[0].main}`;
+    weatherDescription.innerText = `Description: ${data.weather[0].description}`;
+    temp.innerText = `Temperature (Celsius): ${data.main.temp}`;
+    wind.innerText = `Wind Speed (meter/second): ${data.wind.speed}`;
 });
 
-weather.innerText = `${weatherDataMain}`;
-weatherDescription.innerText = `${weatherDataDescription}`;
--- data.js
+++ data.js
@@ -1,15 +1,20 @@
 let cityURL = 'wellington'
-let weatherURL = `api.openweathermap.org/data/2.5/weather?q=${cityURL}&appid=a961fdb45b164f313ff1cfdc95b4c5fa`
+let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityURL}&units=metric&appid=a961fdb45b164f313ff1cfdc95b4c5fa`
 
 const getData = async () => {
     const response = await fetch (weatherURL, {})
     if(response.status === 200 || response.status === 204){
         const data = await response.json()
-        console.log(data)
+        // console.log(data);
+        // console.log(data.name);
+        // console.log(data.sys.country);
+        // console.log(data.main.temp);
+        // console.log(data.main.feels_like);
+        // console.log(data.weather[0].main);
+        // console.log(data.weather[0].description);
+        // console.log(`Wind Speed: ${data.wind.speed}`);
         return data
     } else {
         throw new Error ('Data could not be found!')
     }
 }
-
-getData();
