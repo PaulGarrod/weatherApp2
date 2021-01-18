3ca4c8d8bd23dd4f99b0145235e42842e4c8cf73
Paul Garrod <58899433+PaulGarrod@users.noreply.github.com>
2021-01-18 20:58:17 +1300

    launch app
    app updated ready to launch

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 const city = document.querySelector('#city');
-const icon = document.querySelector('#icon');
 const weather = document.querySelector('#weather');
 const weatherDescription = document.querySelector('#weatherDescription');
 const temp = document.querySelector('#temp');
 const wind = document.querySelector('#wind');
 const visability = document.querySelector('#visability');
+const container = document.querySelector('.container');
 
-//convert temp to oC
+const weatherImages = {
+    clear: 'https://images.unsplash.com/photo-1491308056676-205b7c9a7dc1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzB8fGNsZWFyJTIwc2t5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    clouds: 'https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNsb3VkfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    rain: 'https://images.unsplash.com/photo-1487762488615-8a011458b53e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fHdpbmRvdyUyMHJhaW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    thunderstorm: 'https://images.unsplash.com/photo-1510936723039-063b76afd29e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8ODJ8fHRodW5kZXJ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    snow: 'https://images.unsplash.com/photo-1552426106-891da94e1cf1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fHNub3d8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    mist: 'https://images.unsplash.com/photo-1510596713412-56030de252c8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+}
 
-//convert wind speed?
+const setImg = () => {
+    getData().then((data) => {
+        if (data.weather[0].main.toLowerCase() === 'clear'){
+            container.style.backgroundImage = `url('${weatherImages.clear}')`;
+        } else if (data.weather[0].main.toLowerCase() == 'clouds'){
+            container.style.backgroundImage = `url('${weatherImages.clouds}')`;
+        } else if (data.weather[0].main.toLowerCase() === 'rain' || data.weather[0].main === 'drizzle'){
+            container.style.backgroundImage = `url('${weatherImages.rain}')`;
+        } else if (data.weather[0].main.toLowerCase() === 'thunderstonrm'){
+            container.style.backgroundImage = `url('${weatherImages.thunderstorm}')`;
+        } else if (data.weather[0].main.toLowerCase() === 'snow'){
+            container.style.backgroundImage = `url('${weatherImages.snow}')`;
+        } else if (data.weather[0].main.toLowerCase() === 'mist'){
+            container.style.backgroundImage = `url('${weatherImages.mist}')`;
+        } else {
+            container.style.backgroundImage = `url('${weatherImages.clear}')`;
+        } 
+    })
+}
 
-//convert
-
-const renderWeatherData = getData().then((data) => {
-    city.innerText = `${data.name}, ${data.sys.country}`;
-    weather.innerText = `Current Weather: ${data.weather[0].main}`;
-    weatherDescription.innerText = `Description: ${data.weather[0].description}`;
-    temp.innerText = `Temperature (Celsius): ${data.main.temp}`;
-    wind.innerText = `Wind Speed (meter/second): ${data.wind.speed}`;
-});
-
+const renderData = () => {
+    getData().then((data) => {
+        city.innerText = `${data.name}, ${data.sys.country}`;
+        weather.innerText = `Current Weather: ${data.weather[0].main}`;
+        weatherDescription.innerText = `Description: ${data.weather[0].description}`;
+        temp.innerText = `Temperature (Celsius): ${data.main.temp}`;
+        tempFeels.innerText = `Feels like: ${data.main.feels_like}`;
+        wind.innerText = `Wind Speed (mps): ${data.wind.speed}`;
+        console.log(data.weather[0].main);
+        setImg();
+    });
+}
--- data.js
+++ data.js
@@ -1,20 +1,28 @@
 let cityURL = 'wellington'
-let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityURL}&units=metric&appid=a961fdb45b164f313ff1cfdc95b4c5fa`
 
 const getData = async () => {
     const response = await fetch (weatherURL, {})
     if(response.status === 200 || response.status === 204){
         const data = await response.json()
-        // console.log(data);
-        // console.log(data.name);
-        // console.log(data.sys.country);
-        // console.log(data.main.temp);
-        // console.log(data.main.feels_like);
-        // console.log(data.weather[0].main);
-        // console.log(data.weather[0].description);
-        // console.log(`Wind Speed: ${data.wind.speed}`);
         return data
     } else {
+        // alert('Oops, seems we cant find that city!');
         throw new Error ('Data could not be found!')
     }
 }
+
+const newData = function(chosenCity){
+    weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&units=metric&appid=a961fdb45b164f313ff1cfdc95b4c5fa`
+    getData();
+}
+
+const setCity = document.querySelector('#setCity');
+setCity.addEventListener('change', () => {
+    newData(setCity.value);
+    renderData();
+    setCity.value = '';
+});
+
+newData(cityURL)
+
+renderData();
